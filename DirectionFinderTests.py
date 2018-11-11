b052cc1e408765ccad47c42ccb754a27fb4fd378
patrick <plyons72@gmail.com>
2018-11-11 17:11:25 -0500

    Preliminary unit test setup for remaining tests

--- DirectionFinderTests.py
+++ DirectionFinderTests.py
@@ -23,18 +23,80 @@ class DirectionFinderTests(unittest.TestCase):
         #self.assertEqual(direction[1], 0)
 
     def testTwoIndependentMaxes(self):
-        assert False
+        sensorData = []
+        sensorData.append({'amp': 256, 'location': (1, 1)})
+        sensorData.append({'amp': 128, 'location': (2, 2)})
+        sensorData.append({'amp': 128, 'location': (3, 3)})
+        sensorData.append({'amp': 128, 'location': (4, 4)})
+        sensorData.append({'amp': 128, 'location': (5, 5)})
+        sensorData.append({'amp': 256, 'location': (6, 6)})
+        sensorData.append({'amp': 128, 'location': (7, 7)})
+        sensorData.append({'amp': 128, 'location': (8, 8)})
+        testDF = DirectionFinder
+        direction = testDF.FindDirection(sensorData)
+
+        self.assertEqual(direction, (0, 0))
+
 
     def testSingleMax(self):
-        assert False
+        sensorData = []
+        sensorData.append({'amp': 128, 'location': (1, 1)})
+        sensorData.append({'amp': 128, 'location': (2, 2)})
+        sensorData.append({'amp': 128, 'location': (3, 3)})
+        sensorData.append({'amp': 256, 'location': (4, 4)})
+        sensorData.append({'amp': 128, 'location': (5, 5)})
+        sensorData.append({'amp': 128, 'location': (6, 6)})
+        sensorData.append({'amp': 128, 'location': (7, 7)})
+        sensorData.append({'amp': 128, 'location': (8, 8)})
+        testDF = DirectionFinder
+        direction = testDF.FindDirection(sensorData)
+
+        self.assertEqual(direction, (4, 4))
 
     def testAllZeroAmplitude(self):
-        assert False
+        sensorData = []
+        sensorData.append({'amp': 0, 'location': (1, 1)})
+        sensorData.append({'amp': 0, 'location': (2, 2)})
+        sensorData.append({'amp': 0, 'location': (3, 3)})
+        sensorData.append({'amp': 0, 'location': (4, 4)})
+        sensorData.append({'amp': 0, 'location': (5, 5)})
+        sensorData.append({'amp': 0, 'location': (6, 6)})
+        sensorData.append({'amp': 0, 'location': (7, 7)})
+        sensorData.append({'amp': 0, 'location': (8, 8)})
+        testDF = DirectionFinder
+        direction = testDF.FindDirection(sensorData)
+
+        self.assertEqual(direction, [])
 
     def testOneInvalidAmplitude(self):
-        assert False
+        sensorData = []
+        sensorData.append({'amp': 128, 'location': (1, 1)})
+        sensorData.append({'amp': 128, 'location': (2, 2)})
+        sensorData.append({'amp': 'Q', 'location': (3, 3)})
+        sensorData.append({'amp': 128, 'location': (4, 4)})
+        sensorData.append({'amp': 128, 'location': (5, 5)})
+        sensorData.append({'amp': 128, 'location': (6, 6)})
+        sensorData.append({'amp': 128, 'location': (7, 7)})
+        sensorData.append({'amp': 128, 'location': (8, 8)})
+        testDF = DirectionFinder
+        direction = testDF.FindDirection(sensorData)
+
+        self.assertEqual(direction, (0, 0))
+
 
     def testFewerSensors(self):
+        sensorData = []
+        sensorData.append({'amp': 128, 'location': (1, 1)})
+        sensorData.append({'amp': 128, 'location': (2, 2)})
+        sensorData.append({'amp': 128, 'location': (3, 3)})
+        sensorData.append({'amp': 128, 'location': (4, 4)})
+        sensorData.append({'amp': 128, 'location': (5, 5)})
+        sensorData.append({'amp': 128, 'location': (6, 6)})
+        sensorData.append({'amp': 128, 'location': (7, 7)})
+        testDF = DirectionFinder
+        direction = testDF.FindDirection(sensorData)
+
+        self.assertEqual(direction, (0, 0))
         assert False
 
 if __name__ == "__main__":
