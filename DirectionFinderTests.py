249b87a46e88798c71d5dbcbbd77838edd571e3b
Lindsey <marralindseyj@gmail.com>
2018-11-11 15:51:30 -0500

    implements unit test for all elements identical

--- DirectionFinderTests.py
+++ DirectionFinderTests.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 import unittest
-import DirectionFinder
+from DirectionFinder import DirectionFinder
 
 
 class DirectionFinderTests(unittest.TestCase):
 
     def testAllSensorsIdentical(self):
-        assert 0 == 1
+        sensorData = []
+        sensorData.append({'amp':128, 'location': (1,1)})
+        sensorData.append({'amp':128, 'location': (2,2)})
+        sensorData.append({'amp':128, 'location': (3,3)})
+        sensorData.append({'amp':128, 'location': (4,4)})
+        sensorData.append({'amp':128, 'location': (5,5)})
+        sensorData.append({'amp':128, 'location': (6,6)})
+        sensorData.append({'amp':128, 'location': (7,7)})
+        sensorData.append({'amp':128, 'location': (8,8)})
+        testDF = DirectionFinder
+        direction = testDF.FindDirection(sensorData)
+        
+        self.assertEqual(direction, (0,0))
+        #self.assertEqual(direction[1], 0)
 
     def testTwoIndependentMaxes(self):
         assert False
