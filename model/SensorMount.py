6c8388af49004b7c6551c88e650df0a104716b6b
Joshua Lamiano <jlamiano@zoll.com>
2018-11-17 17:33:03 -0500

    SensorMount: Adds an object to aggregate and control the location of Sensor objects.

---
+++ model/SensorMount.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+A container for LightSensors which provides a fixed physical frame for the
+LightSensors on be attached to so that moving the SensorMount affects all
+attached LightSensors appropriately.
+
+SensorMounts allow translations and rotations when specifying a move. Thus
+they do not allow for six degrees of freedom of movement. This is sufficient
+for modeling the movements of a cart, but would not be good enough for a
+flying drone.
+"""
+
+import math
+
+class SensorMount(object):
+    """
+    Represent an apparatus on which sensors may be mounted such that as
+    the SensorMount is moved, the attached sensors are also moved.
+    """
+    def __init__(self):
+        """
+        Initialize a SensorMount object at the origin with no attached sensors
+        """
+        self.sensor_list = []
+        self.location = (0, 0, 0)
+        self.rotation = 0
+
+    def add_new_sensor(self, sensor):
+        """
+        Attach a new sensor object to the SensorMount
+        """
+        self.sensor_list.append(sensor)
+
+    def move_to_position(self, location, rotation):
+        """
+        Move this SensorMount as well as all the attached sensors to
+        the new location defined by the provided translation and rotation
+        """
+        rotation_rads = math.radians(rotation)
+
+        for index, sensor in enumerate(self.sensor_list):
+            current_position = sensor.current_position()
+            relative_x = current_position[0] - self.location[0]
+            relative_y = current_position[1] - self.location[1]
+            relative_z = current_position[2] - self.location[2]
+            # The rotation is calculated about the mount position
+            rotated_x = relative_x * math.cos(rotation_rads) \
+                + relative_y * math.sin(rotation_rads)
+
+            rotated_y = - relative_x * math.sin(rotation_rads) \
+                + relative_y * math.cos(rotation_rads)
+
+            rotated_z = relative_z
+
+            # Translate back to absolute coordinates
+            rotated_position = (rotated_x, rotated_y, rotated_z) + self.location
+
+            # The translation is the difference in the coordinates between
+            # the initial and final position.
+
+
+            new_x = rotated_position[0] \
+                + (location[0] - self.location[0])
+            new_y = rotated_position[1] \
+                + (location[1] - self.location[1])
+            new_z = rotated_position[2] \
+                + (location[2] - self.location[2])
+
+            new_sensor_position = (new_x, new_y, new_z)
+            sensor.move_to_position(new_sensor_position)
+            self.sensor_list[index] = sensor
+
+        self.location = location
+    
+    def get_sensor(self, sensor_index):
+        return self.sensor_list[sensor_index]
+    
+
+    def current_position(self):
+        """
+        Query for the current position of the SensorMount
+        """
+        return self.location
---
+++ model/test/SensorMountTests.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+"""
+Contains unit tests for SensorMount object
+"""
+import sys
+import unittest
+sys.path.append("..")
+from LightSensor import LightSensor
+from SensorMount import SensorMount
+
+class SensorMountTests(unittest.TestCase):
+    """
+    SensorMount unit test fixture
+    """
+    def testSimpleTranslation(self):
+        """
+        Confirm that a SensorMount correctly reports its own position
+        """
+        mount_under_test = SensorMount()
+
+        mount_location = (1, 2, 3)
+        mount_under_test.move_to_position(mount_location, 0)
+        self.assertEqual(mount_under_test.current_position(), mount_location)
+
+    def testSimpleTranslationWithSensor(self):
+        """
+        Confirm that a SensorMount with a single sensor properly translates its
+        Sensor object's location when it is moved without a rotation
+        """
+        mount_under_test = SensorMount()
+
+        sensor_location = (1, 0, 0)
+        attached_sensor = LightSensor(sensor_location, (), ())
+
+        mount_location = (1, 2, 3)
+        mount_under_test.add_new_sensor(attached_sensor)
+        mount_under_test.move_to_position(mount_location, 0)
+        self.assertEqual(mount_under_test.get_sensor(0).current_position(), (2, 2, 3))
+
+    def test90DegreRotation(self):
+        """
+        Confirm that a SensorMount with a single sensor properly rotates its
+        Sensor object's location when it is rotated 90 degrees without a
+        translation.
+        """
+        mount_under_test = SensorMount()
+
+        sensor_location = (1, 0, 0)
+        attached_sensor = LightSensor(sensor_location, (), ())
+
+        mount_location = (0, 0, 0)
+        rotated_location = (0, -1, 0)
+        mount_under_test.add_new_sensor(attached_sensor)
+        mount_under_test.move_to_position(mount_location, 90)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[0],\
+            rotated_location[0], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[1],\
+            rotated_location[1], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[2],\
+            rotated_location[2], 6)
+
+    def test180DegreeRotation(self):
+        """
+        Confirm that a SensorMount with a single sensor properly rotates its
+        Sensor object's location when it is rotated 180 degrees without a
+        translation.
+        """
+        mount_under_test = SensorMount()
+
+        sensor_location = (1, 0, 0)
+        attached_sensor = LightSensor(sensor_location, (), ())
+
+        mount_location = (0, 0, 0)
+        rotated_location = (-1, 0, 0)
+        mount_under_test.add_new_sensor(attached_sensor)
+        mount_under_test.move_to_position(mount_location, 180)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[0],\
+            rotated_location[0], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[1],\
+            rotated_location[1], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[2],\
+            rotated_location[2], 6)
+
+    def test270DegreeRotation(self):
+        """
+        Confirm that a SensorMount with a single sensor properly rotates its
+        Sensor object's location when it is rotated 270 degrees without a
+        translation.
+        """
+        mount_under_test = SensorMount()
+
+        sensor_location = (1, 0, 0)
+        attached_sensor = LightSensor(sensor_location, (), ())
+
+        mount_location = (0, 0, 0)
+        rotated_location = (0, 1, 0)
+        mount_under_test.add_new_sensor(attached_sensor)
+        mount_under_test.move_to_position(mount_location, 270)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[0],\
+            rotated_location[0], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[1],\
+            rotated_location[1], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[2],\
+            rotated_location[2], 6)
+
+    def test360DegreeRotation(self):
+        """
+        Confirm that a SensorMount with a single sensor properly rotates its
+        Sensor object's location when it is rotated 360 degrees without a
+        translation.
+        """
+        mount_under_test = SensorMount()
+
+        sensor_location = (1, 0, 0)
+        attached_sensor = LightSensor(sensor_location, (), ())
+
+        mount_location = (0, 0, 0)
+        rotated_location = (1, 0, 0)
+        mount_under_test.add_new_sensor(attached_sensor)
+        mount_under_test.move_to_position(mount_location, 360)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[0],\
+            rotated_location[0], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[1],\
+            rotated_location[1], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[2],\
+            rotated_location[2], 6)
+
+    def testTranslationAndRotationWithSensor(self):
+        """
+        Confirm that a SensorMount with a single sensor properly updates its
+        Sensor's location when the SensorMount is both rotated and translated.
+        """
+        mount_under_test = SensorMount()
+
+        sensor_location = (100, 0, 0)
+        attached_sensor = LightSensor(sensor_location, (), ())
+
+        mount_location = (2, 5, 7)
+        rotated_location = (86.804809616, -47.991926423, 7)
+        mount_under_test.add_new_sensor(attached_sensor)
+        mount_under_test.move_to_position(mount_location, 32)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[0],\
+            rotated_location[0], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[1],\
+            rotated_location[1], 6)
+        self.assertAlmostEqual(mount_under_test.get_sensor(0).current_position()[2],\
+            rotated_location[2], 6)
+
+    def testMoveAndRotateMountWithMultipleSensors(self):
+        """
+        Confirm that a SensorMount with a multiple sensors properly updates its
+        Sensor's location when the SensorMount is both rotated and translated.
+        """
+        mount_under_test = SensorMount()
+
+        attached_sensor = []
+        attached_sensor.append(LightSensor((100, 0, 0), (), ()))
+        attached_sensor.append(LightSensor((70.710678119, 70.710678119, 0), (), ()))
+        attached_sensor.append(LightSensor((0, 100, 0), (), ()))
+        attached_sensor.append(LightSensor((-70.710678119, 70.710678119, 0), (), ()))
+        attached_sensor.append(LightSensor((-100, 0, 0), (), ()))
+        attached_sensor.append(LightSensor((-70.710678119, -70.710678119, 0), (), ()))
+        attached_sensor.append(LightSensor((0, -100, 0), (), ()))
+        attached_sensor.append(LightSensor((70.710678119, -70.710678119, 0), (), ()))
+
+        mount_location = (2, 5, 7)
+        rotated_location = []
+        rotated_location.append((72.710678119, -65.710678119, 7))
+        rotated_location.append((102, 5, 7))
+        rotated_location.append((72.710678119, 75.710678119, 7))
+        rotated_location.append((2, 105, 7))
+        rotated_location.append((-68.710678119, 75.710678119, 7))
+        rotated_location.append((-98, 5, 7))
+        rotated_location.append((-68.710678119, -65.710678119, 7))
+        rotated_location.append((72.710678119, -65.710678119, 7))
+
+        mount_under_test.add_new_sensor(attached_sensor[0])
+        mount_under_test.add_new_sensor(attached_sensor[1])
+        mount_under_test.add_new_sensor(attached_sensor[2])
+        mount_under_test.add_new_sensor(attached_sensor[3])
+        mount_under_test.add_new_sensor(attached_sensor[4])
+        mount_under_test.add_new_sensor(attached_sensor[5])
+        mount_under_test.add_new_sensor(attached_sensor[6])
+        mount_under_test.add_new_sensor(attached_sensor[7])
+
+        mount_under_test.move_to_position(mount_location, 45)
+        for i in range(0, 7):
+            self.assertAlmostEqual(mount_under_test.get_sensor(i).current_position()[0],\
+                rotated_location[i][0], 6)
+            self.assertAlmostEqual(mount_under_test.get_sensor(i).current_position()[1],\
+                rotated_location[i][1], 6)
+            self.assertAlmostEqual(mount_under_test.get_sensor(i).current_position()[2],\
+                rotated_location[i][2], 6)
+
+if __name__ == "__main__":
+    unittest.main()
